99f87180cb239ef8c12dd33935d7e484297dea12
Alex Stéphane NGOMBOL <gpetitalex10@gmail.com>
2020-09-08 19:48:13 +0100

    Improve web view

--- app/src/main/java/com/mmac/app/SplashScreen.kt
+++ app/src/main/java/com/mmac/app/SplashScreen.kt
@@ -1,11 +1,23 @@
 package com.mmac.app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        logo.alpha = 0f
+        // Wait 1500 ms
+        logo.animate().setDuration(1500).alpha(1f).withEndAction {
+            // Start app & close splash screen with transition
+            val mainActivityIntent = Intent(baseContext, MainActivity::class.java);
+            startActivity(mainActivityIntent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
     }
 }

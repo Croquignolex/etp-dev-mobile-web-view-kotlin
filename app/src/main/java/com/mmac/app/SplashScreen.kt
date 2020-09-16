30d75eda8ed1a4424ca08b25bee8c5f5e93ef8a8
Alex Stéphane NGOMBOL <gpetitalex10@gmail.com>
2020-09-16 13:44:11 +0100

    Improve Developer name

--- app/src/main/java/com/mmac/app/SplashScreen.kt
+++ app/src/main/java/com/mmac/app/SplashScreen.kt
@@ -2,13 +2,18 @@ package com.mmac.app
 
 import android.os.Bundle
 import android.content.Intent
+import android.text.method.LinkMovementMethod
 import androidx.appcompat.app.AppCompatActivity
+import com.mmac.app.extensions.removeLinksUnderline
 import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        developer.movementMethod = LinkMovementMethod.getInstance();
+        developer.removeLinksUnderline()
 
         logo.alpha = 0f
         // Wait 1500 ms
---
+++ app/src/main/java/com/mmac/app/extensions/removeLinksUnderline.kt
@@ -0,0 +1,19 @@
+package com.mmac.app.extensions
+
+import android.text.TextPaint
+import android.widget.TextView
+import android.text.style.URLSpan
+import android.text.SpannableString
+
+fun TextView.removeLinksUnderline() {
+    val spannable = SpannableString(text)
+    for (u in spannable.getSpans(0, spannable.length, URLSpan::class.java)) {
+        spannable.setSpan(object : URLSpan(u.url) {
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.isUnderlineText = false
+            }
+        }, spannable.getSpanStart(u), spannable.getSpanEnd(u), 0)
+    }
+    text = spannable
+}
